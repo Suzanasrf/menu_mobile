022d28095410373c646a34a2c90c215f873d54ab
Suzanasrf <56602037+Suzanasrf@users.noreply.github.com>
2022-05-16 16:26:56 -0300

    menumobile

---
+++ menu-mobile.js
@@ -0,0 +1,18 @@
+
+
+export default function initMenuMobile(){
+
+}
+
+const menuButton = document.querySelector('[data-menu="button"]');
+
+const menuList = document.querySelector('[data-menu="list"]');
+
+function openMenu(event){
+
+    menuButton.classList.add('active');
+    menuList.classList.add('active')
+
+}
+
+menuButton.addEventListener('click', openMenu);
